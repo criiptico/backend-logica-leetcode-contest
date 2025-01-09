724b54d03b5851bb48ab475409a03c2113d31470
criiptico <adrianquiroz166@gmail.com>
2025-01-09 08:27:29 -0600

    Tested forgot-password and change-password endpoints

--- index.js
+++ index.js
@@ -28,77 +28,18 @@ const supabase = createClient(
   process.env.SB_ANON_PUBLIC_KEY
 );
 
-function generateOTP() {
-  // Generates a unique token every 30 seconds
-  const otp = speakeasy.totp({
-    secret: process.env.OTP_SECRET,
-    encoding: "base32",
-  });
-  return otp;
-}
-
 app.get("/logica-leetcode/v1/", (req, res) => {
   res.send("Welcome to the LOGICA Leetcode Contest Backend API");
 });
 
-async function sendOTPEmail(email, otp) {
-  let transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: process.env.LOGICA_DEV_EMAIL,
-      pass: process.env.LOGICA_DEV_APP_PASSWORD,
-    },
-  });
-
-  let info = await transporter.sendMail({
-    from: `"LOGICA Development" ${process.env.LOGICA_DEV_EMAIL}`,
-    to: email,
-    subject: "Your OTP Code For LOGICA Leetcode Contest",
-    text: `Your OTP Code is: ${otp}\n\nClick this link to be redirected to the sign in page: https://google.com`,
-  });
-
-  console.log(`Message sent: ${info.messageId}`);
-}
-
-app.post("/logica-leetcode/v1/generate-otp/:role", async (req, res) => {
-  const { email } = req.body;
-  const { role: role } = req.params;
-
-  if (!email) {
-    res.status(400).send({ error: "Email is required" });
-  }
-  if (role != "organizer" || role != "participant") {
-    res.status(400).send({ error: "Appropriate role is required" });
-  }
-
-  const otp = generateOTP();
-  try {
-    await sendOTPEmail(email, otp);
-
-    const { data, error } = await supabase
-      .from(`${role}`)
-      .update([{ one_time_password: otp }])
-      .eq("email", email);
-
-    if (error) {
-      res.status(500).send({
-        error: "Failed to set OTP to the database",
-        message: error.message,
-      });
-    }
-    res.status(200).json({ message: "success" });
-  } catch (error) {
-    res
-      .status(500)
-      .json({ error: "Failed to send OTP email", message: error.message });
-  }
-});
+// RESTful API calls
+// ----
 
 app.get("/logica-leetcode/v1/problems", async (req, res) => {
   const { data, error } = await supabase.from("problem").select();
   if (error) {
     console.log(error.message);
-    res.send({ error: "An error has occured, please check the console log." });
+    res.send({ error: "An error has occured" });
   } else {
     res.send(data);
   }
@@ -132,7 +73,7 @@ app.put("/logica-leetcode/v1/problem", async (req, res) => {
     }
   } else {
     res.send({
-      Error: "Body must contain all key-value pairs.",
+      error: "Body must contain all key-value pairs.",
     });
   }
 });
@@ -141,99 +82,34 @@ app.delete("/logica-leetcode/v1/problem/:id", async (req, res) => {
   // const { problem_id: problem_id } = req.body;
   const { id: problem_id } = req.params;
 
-  if (problem_id != null) {
-    const { data, error } = await supabase
-      .from("problem")
-      .delete()
-      .eq("problem_id", problem_id);
+  try {
+    if (problem_id != null) {
+      const { data, error } = await supabase
+        .from("problem")
+        .delete()
+        .eq("problem_id", problem_id);
 
-    if (error) {
-      console.log(error.message);
-      res.send({
-        Error: "An error has occured, please check the console log.",
-      });
+      if (error) {
+        console.log(error.message);
+        res.send({
+          Error: "An error has occured, please check the console log.",
+        });
+      } else {
+        res.send({ Result: "Success" });
+      }
     } else {
-      res.send({ Result: "Success" });
+      res.send({
+        error: "Body must contain all key-value pairs.",
+      });
     }
-  } else {
-    res.send({
-      Error: "Body must contain all key-value pairs.",
-    });
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An unexpected error occurred" });
   }
 });
 
-// NEEDS TESTING
-// app.get("/logica-leetcode/v1/contest/problems", async (req, res) => {
-//   const { semester: semester, year: year } = req.query;
-
-//   if (semester != null || year != null) {
-//     // Needs testing. Expected to return all problems of a contest_id, where they are all from a certain semester and year
-//     const { data, error } = await supabase
-//       .from("contest")
-//       .select(
-//         `
-//         *,
-//         contest_problem (
-//         contest_id,
-//         problem_id,
-//           problem(
-//             problem_id,
-//             problem_name,
-//             difficulty,
-//             url
-//           )
-//         )`
-//       )
-//       .eq(`contest.semester`, semester)
-//       .eq(`contest.year`, year);
-
-//     if (error) {
-//       console.log(error.message);
-//       res.send({
-//         Error: "An error has occured, please check the console log.",
-//       });
-//     } else {
-//       res.send(data);
-//     }
-//   } else {
-//     res.send({ Error: "Request line must contain all query parameters." });
-//   }
-// });
-
-// NEEDS TESTING
-// app.get("/logica-leetcode/v1/leaderboard", async (req, res) => {
-//   const { semester: semester, year: year } = req.query;
-
-//   if (semester != null || year != null) {
-//     // Needs testing. Should return all of the group names, participant names, and points for that group given the
-//     // contest's semester and year
-//     const { data, error } = await supabase
-//       .from("contest")
-//       .select(
-//         `
-//       *,
-//       group(
-//         group_name,
-//         participant_names,
-//         points
-//       )`
-//       )
-//       .eq(`contest.semester`, semester)
-//       .eq(`contest.year`, year);
-
-//     if (error) {
-//       console.log(error.message);
-//       res.send({
-//         Error: "An error has occured, please check the console log.",
-//       });
-//     } else {
-//       res.send(data);
-//     }
-//   } else {
-//     res.send({ Error: "Request line must contain all query parameters." });
-//   }
-// });
-
+// User & Password Management
+// ----
 async function register(name, email, password) {
   const saltRounds = 10;
   const hashedPassword = await bcrypt.hash(password, saltRounds);
@@ -254,9 +130,9 @@ async function register(name, email, password) {
 // going to introduce hashing,
 //  currently inserts hard coded details
 app.get("/register", async (req, res) => {
-  let name = "Ivan";
-  let email = "itorr4@uic.edu";
-  let password = "i-am-password";
+  let name = "Adrian";
+  let email = "aquiro20@uic.edu";
+  let password = "_the_dummy_password_";
 
   try {
     const { data, error } = await supabase
@@ -320,8 +196,11 @@ app.get("/delete-user", async (req, res) => {
 });
 
 app.get("/login", async (req, res) => {
-  let email = "itorr4@uic.edu";
-  let password = "i-am-password";
+  // let email = "itorr4@uic.edu";
+  // let password = "i-am-password";
+
+  let email = "aquiro20@uic.edu";
+  let password = "_the_dummy_password_";
 
   try {
     // Fetch user by email
@@ -357,15 +236,151 @@ app.get("/login", async (req, res) => {
   }
 });
 
-// Used for local testing
-// app.get("/peek-dotenv", (req, res) => {
-//   console.log(
-//     `Project URL: ${process.env.SB_PROJECT_URL}\nAnon Public Key: ${process.env.SB_ANON_PUBLIC_KEY}`
-//   );
-//   res.send(
-//     `Project URL: ${process.env.SB_PROJECT_URL}\nAnon Public Key: ${process.env.SB_ANON_PUBLIC_KEY}`
-//   );
-// });
+function generateOTP() {
+  // Generates a unique token every 30 seconds
+  const otp = speakeasy.totp({
+    secret: process.env.OTP_SECRET,
+    encoding: "base32",
+  });
+  return otp;
+}
+
+async function sendOTPEmail(email, otp) {
+  let transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: process.env.LOGICA_DEV_EMAIL,
+      pass: process.env.LOGICA_DEV_APP_PASSWORD,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: `"LOGICA Development" ${process.env.LOGICA_DEV_EMAIL}`,
+    to: email,
+    subject: "Your OTP Code For LOGICA Leetcode Contest",
+    text: `Your OTP Code is: ${otp}\n\nClick this link to be redirected to the sign in page: https://google.com`, // This is a placeholder, must replace later
+  });
+
+  console.log(`Message sent: ${info.messageId}`);
+}
+
+app.post("/logica-leetcode/v1/forgot-password/:role", async (req, res) => {
+  const { email } = req.body;
+  const { role: role } = req.params;
+
+  if (!email) {
+    res.status(400).send({ error: "Email is required" });
+  }
+  if (role == "organizer" || role == "participant") {
+    const otp = generateOTP();
+    try {
+      const saltRounds = 10;
+      const hashed_otp = await bcrypt.hash(otp, saltRounds);
+
+      const { data, error } = await supabase
+        .from(`${role}`)
+        .update([{ one_time_password: hashed_otp }])
+        .eq("participant_email", email);
+
+      if (error) {
+        res.status(500).send({
+          error: "Failed to set OTP to the database",
+          message: error.message,
+        });
+      }
+      await sendOTPEmail(email, otp);
+      res.status(200).send({ message: "success" });
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ error: "An unexpected error occurred" });
+    }
+  } else {
+    res
+      .status(400)
+      .send({ error: "Appropriate role is required", message: `${role}` });
+  }
+});
+
+async function reset_password(email, password) {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+  try {
+    const { pass_data, pass_error } = await supabase
+      .from("participant")
+      .update([
+        {
+          password: hashedPassword,
+        },
+      ])
+      .eq("participant_email", email);
+
+    const { otp_data, otp_error } = await supabase
+      .from("participant")
+      .update([
+        {
+          one_time_password: null,
+        },
+      ])
+      .eq("participant_email", email);
+    if (pass_error) {
+      throw new Error(pass_error.message);
+    }
+    if (otp_error) {
+      throw new Error(otp_error.message);
+    }
+  } catch (error) {
+    throw new Error(error.message);
+  }
+}
+
+app.get("/change-password", async (req, res) => {
+  // Used for testing, use body payload in production
+  let email = "aquiro20@uic.edu";
+  let new_password = "what_is_going_on_here";
+  let one_time_password = "145046"; // Must change after each new forgot-password request is made
+
+  try {
+    // Fetch user by email
+    const { data, error } = await supabase
+      .from("participant")
+      .select("participant_name, participant_email, one_time_password")
+      .eq("participant_email", email);
+
+    if (error) {
+      return res.status(500).json({ error: error.message });
+    }
+
+    // Check if user exists
+    if (!data || data.length === 0) {
+      return res.status(404).json({ error: "User not found" });
+    }
+
+    // user entry
+    const user = data[0];
+
+    // Checking if one_time_password has been set or not
+    if (user.one_time_password) {
+      // Check if the otp is correct
+      const isCorrectOTP = await bcrypt.compare(
+        one_time_password,
+        user.one_time_password
+      );
+      if (!isCorrectOTP) {
+        return res
+          .status(401)
+          .json({ error: "Invalid email or one time password" });
+      } else {
+        // If otp is correct, then set the password column to the new password
+        await reset_password(email, new_password);
+        res.json({ message: "Email reset successfully" });
+      }
+    }
+  } catch (err) {
+    console.error(err);
+    res.status(500).json({ error: "An unexpected error occurred" });
+  }
+});
 
 app.listen(port, () => {
   console.log(
